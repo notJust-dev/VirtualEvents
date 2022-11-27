226f03ce20cb2a39bbda03dfd01b0c751ba89dfe
Vadim Savin <savinvadim1312@gmail.com>
2022-11-27 16:08:13 +0000

    Connect to stream chat sdk and create a user

--- context/ChatContext.tsx
+++ context/ChatContext.tsx
@@ -1,8 +1,48 @@
-import React, { createContext, useContext } from "react";
+import { useUserData } from "@nhost/react";
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { StreamChat } from "stream-chat";
 
 export const ChatContext = createContext({});
 
 const ChatContextProvider = ({ children }: { children: React.ReactNode }) => {
+  // component
+  const [chatClient, setChatClient] = useState<StreamChat>();
+
+  const user = useUserData();
+
+  useEffect(() => {
+    const initChat = async () => {
+      if (!user) {
+        return;
+      }
+
+      const client = StreamChat.getInstance("249ewtgkuz9h");
+
+      // get information about the authenticated
+      // connect the user to stream chat
+      await client.connectUser(
+        {
+          id: user.id,
+          name: user.displayName,
+          image: user.avatarUrl,
+        },
+        client.devToken(user.id)
+      );
+
+      setChatClient(client);
+    };
+
+    initChat();
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      if (chatClient) {
+        chatClient.disconnectUser();
+      }
+    };
+  }, []);
+
   const value = { username: "Vadim" };
   return <ChatContext.Provider value={value}>{children}</ChatContext.Provider>;
 };
